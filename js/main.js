46ec41c183a0b73e286a8dbfb73fb223f5fb4632
Lida Deineko <93deineko@gmail.com>
2022-08-09 11:16:48 +0300

    Add extra tasks: animation, sticky, drop menu

--- js/main.js
+++ js/main.js
@@ -11,4 +11,32 @@ window.addEventListener('load', () => {
 
     menuBtn.addEventListener('click', showMenu)
 
+
+
+    //shadow for header
+    let header = document.querySelector('.header');
+    window.addEventListener('scroll', () => {
+        if(window.scrollY > 50){
+            header.classList.add('shadow')
+        } else if (window.scrollY < 50){
+            header.classList.remove('shadow')
+        }
+    })
+
+
+    // animated move-left
+
+    const postRight = document.querySelector('.post-right')
+
+    const postRightRect = postRight.getBoundingClientRect()
+    console.dir(postRightRect.y)
+    console.dir(postRight)
+
+    const valHeight = postRightRect.y-postRight.clientHeight-400
+
+    window.addEventListener('scroll', () => {
+        if(window.scrollY > valHeight){
+            postRight.classList.add('post-right__animated')
+        }
+    })
 })
